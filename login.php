cb10559b9d605a50ad95dde6b8d0fbdf698f5110
hachisnail <jeffersontalagtag06@gmail.com>
2024-12-19 02:43:10 +0800

    small fix

--- login.php
+++ login.php
@@ -13,9 +13,9 @@
     <?php
     // Database connection
     $servername = "localhost";
-    $username = "u143688490_admin	";
+    $username = "u143688490_admin";
     $password = "Fujiwara000!";
-    $dbname = "u143688490_admin	";
+    $dbname = "u143688490_admin";
 
     $conn = new mysqli($servername, $username, $password, $dbname);
 
